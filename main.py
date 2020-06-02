1bfeca672185121eb24ac713daf5d86776083b37
Martín Nieva <martinjafactor@gmail.com>
2020-06-02 12:43:53 -0300

    Polished code, added clean option

--- main.py
+++ main.py
@@ -1,25 +1,54 @@
 from moviepy import editor
 import sys
+import glob
+import os
 
-FILE_NAME = 'toedit.mp4'
 OUTPUT_NAME = 'result.mp4'
+LEN_ARGS = 5
+
+clean_filetypes = 'webm', 'mp4', 'gif'
+
+def clean_files(filetype):
+    for file in glob.glob('*.' + filetype):
+        print('Deleting ' + file + '... ')
+
+        os.remove(file)
+
+        print('Done!')
+
+    return
 
 def main(args):
-    if len(args) != 4:
-        return False, 'Pelotudo! No escribiste los argumentos bien al iniciar el script!'
+    if len(args) != LEN_ARGS:
+        if args[1] == 'clean':
+            print('Cleaning...')
+
+            for filetype in clean_filetypes:
+                clean_files(filetype)
+
+            return True, 'Success!'
+
+        return False, 'Ignacio you dumbass! You havent written the arguments properly! Domaditowwwnnn'
 
     text_size = 25
 
     text_x = args[1]
     text_y = args[2]
     text_color = args[3]
+    video_format = args[4]
 
-    if len(args) == 5:
-        text_size = args[4]
+    for video_file in glob.glob('*.' + video_format):
+        video_name = video_file
+        break
+
+    print('Video: ' + video_name)
+
+    if len(args) == LEN_ARGS + 1:
+        text_size = args[LEN_ARGS]
 
     text_anchor = text_x, text_y
 
-    clip = editor.VideoFileClip(FILE_NAME)
+    clip = editor.VideoFileClip(video_name)
 
     txt_clip = editor.TextClip('@voxed.gram', fontsize=text_size, color=text_color).set_position(text_anchor).set_duration(clip.duration)
 
@@ -32,5 +61,4 @@ def main(args):
 if __name__ == '__main__':
     success, reason = main(sys.argv)
 
-    if not success:
-        print(reason)
+    print(reason)
