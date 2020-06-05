c0fd724143022154f9fce86ede98e5fa2e88d6a3
Martín Nieva <martinjafactor@gmail.com>
2020-06-05 18:54:33 -0300

    Implementing click

--- main.py
+++ main.py
@@ -1,60 +1,10 @@
-from moviepy import editor
-import sys
-import glob
-import os
+import click
 
-OUTPUT_NAME = 'result.mp4'
-LEN_ARGS = 5
-
-clean_filetypes = 'webm', 'mp4', 'gif'
-
-def clean_files(filetype):
-    for file in glob.glob('*.' + filetype):
-        print('Deleting ' + file + '... ')
-
-        os.remove(file)
-
-        print('Done!')
-
+@click.command()
+@click.option('--video', default=False, help='Write this option in case the file you are gonna specify is a video')
+@click.option('--image', default=False, help='Write this option in case the file you are gonna specify is a image')
+@click.argument('filepath')
+def filename(image, video, filepath):
     return
 
-def main(args):
-    if len(args) != LEN_ARGS:
-        if args[1] == 'clean':
-            print('Cleaning...')
-
-            for filetype in clean_filetypes:
-                clean_files(filetype)
-
-            return True, 'Success!'
-
-        return False, 'Ignacio you dumbass! You havent written the arguments properly! Domaditowwwnnn'
-
-    text_size = 25
-
-    text_x = args[1]
-    text_y = args[2]
-    text_color = args[3]
-    video_name = args[4]
-
-    print('Video: ' + video_name)
-
-    if len(args) == LEN_ARGS + 1:
-        text_size = args[LEN_ARGS]
-
-    text_anchor = text_x, text_y
-
-    clip = editor.VideoFileClip(video_name)
-
-    txt_clip = editor.TextClip('@voxed.gram', fontsize=text_size, color=text_color).set_position(text_anchor).set_duration(clip.duration)
-
-    final_clip = editor.CompositeVideoClip([clip, txt_clip])
-
-    final_clip.write_videofile(OUTPUT_NAME)
-
-    return True, 'Success!'
-
-if __name__ == '__main__':
-    success, reason = main(sys.argv)
-
-    print(reason)
+filename()
